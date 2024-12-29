d180dfb074ffbe0ca94d3ce47890a07d594d94b9
saikattyo <katsuya12321@gmail.com>
2024-12-30 00:07:49 +0900

    ファイルなど調整

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Saikattyoの掃溜め';
+export const SITE_TITLE = 'Saikattyoの掃き溜め';
 export const SITE_DESCRIPTION = 'つらつらと';
